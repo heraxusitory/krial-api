29baaa9e1f7f6992c28ffe0ef482f6be76dc2f8c
vladislav <vladiksmart@gmail.com>
2022-07-22 21:04:45 +0300

    fix filter

--- app/Services/Filter.php
+++ app/Services/Filter.php
@@ -87,7 +87,7 @@ class Filter
                 });
             } elseif ($filter['entity_type'] === 'price') {
                 $dg_query->whereHas('traiding_options', function (Builder $query) use ($filter) {
-                    $query->whereIn('price', [(string)$filter['values']['min'], (string)$filter['values']['max']]);
+                    $query->whereIn('price', [(int)$filter['values']['min'], (int)$filter['values']['max']]);
                 });
             }
         }
