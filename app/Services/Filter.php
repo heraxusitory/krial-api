290f2c82e63fb7aef191d941c61ce35d238d4283
vladislav <vladiksmart@gmail.com>
2022-07-22 20:50:34 +0300

    fix filter

--- app/Services/Filter.php
+++ app/Services/Filter.php
@@ -53,7 +53,7 @@ class Filter
                             break;
                         case 'range':
                             if (isset($filter['values']['min']) && isset($filter['values']['max'])) {
-                                $query->whereBetween('slug', [(string)($filter['values']['min']), (string)$filter['values']['max']]);
+                                $query->whereBetween('value', [(string)($filter['values']['min']), (string)$filter['values']['max']]);
                             }
                             break;
                         case 'input':
